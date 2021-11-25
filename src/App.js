df390f014d015b22e66c528e982d4fb447fc5f16
katedelikat <kate.delikat@gmail.com>
2021-11-25 12:46:47 +0200

    Update App.js
    Some change for travis trigger

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Finally working
+          Some change for travis trigger
         </p>
         <a
           className="App-link"
